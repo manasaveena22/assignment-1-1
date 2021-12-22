d744d9eb037a948ca2c18a52d78486b38bc5f6fb
manasaveena22 <92075064+manasaveena22@users.noreply.github.com>
2021-12-22 22:01:42 +0530

    Add files via upload
    assignment:
    Algorithm of the given series, floor division, logical operators, comparison operators

---
+++ assignments 1_1.py
@@ -0,0 +1,64 @@
+
+"""
+ALGORITHM
+step1:start
+step2:get n, n is the even places 
+step3:get m, m is the
+step4:x=x-2
+step5:y=y+2
+step6:stop
+"""
+
+#FLOOR DIVISION#
+a = 11
+b = 2
+print(a//b)  
+
+a = -11
+b = -2
+print(a//b)  
+
+a = 11
+b = -2
+print(a//b) 
+
+a = -11
+b = 2
+print(a//b) 
+
+#LOGIC OPERATORS#
+
+x = 2
+print(x > 1 and x < 10)
+
+x = 2
+print(x > 4 or x < 10)
+
+x= 2
+print(not(x > 4 and x < 10))
+
+#comparision operators#
+
+x = 5
+y = 3
+print(x == y)
+
+x = 5
+y = 3
+print(x != y)
+
+x = 5
+y = 3
+print(x > y)
+
+x = 5
+y = 3
+print(x < y)
+
+x = 5
+y = 3
+print(x <= y)
+
+x = 5
+y = 3
+print(x >= y)
